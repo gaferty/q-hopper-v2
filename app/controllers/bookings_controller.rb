a4466523cc7655731d4923ee53f8ffbb003d84f4
lawrence harlow <lawrenceharlow@gmail.com>
2024-05-13 18:24:59 +0100

    created actions in the controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,28 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :new, :create]
+  def index
 
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
 end
