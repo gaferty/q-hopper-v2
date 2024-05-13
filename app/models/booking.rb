622ed505c3c1760ce44f3a9babe39a4d594e4f67
lawrence harlow <lawrenceharlow@gmail.com>
2024-05-13 15:34:14 +0100

    created validations for the restaurant and bookings model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
+  validates :accepted, default: false
+  validates :completed, default: false
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ApplicationRecord
-  belongs_to :user
+  belongs_to :use
+  validates :name, presence: true
+  validates :cuisine
+  validates :address, presence: true
+  has_many :bookings
 end
