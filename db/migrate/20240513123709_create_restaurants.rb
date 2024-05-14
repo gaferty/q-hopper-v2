47ac5990a39392c8e1a871d2de3730dba1806272
lawrence harlow <lawrenceharlow@gmail.com>
2024-05-14 11:46:08 +0100

    updated the databases to default completed and accepted to false

--- db/migrate/20240513123709_create_restaurants.rb
+++ db/migrate/20240513123709_create_restaurants.rb
@@ -1,13 +1,13 @@
 class CreateRestaurants < ActiveRecord::Migration[7.1]
   def change
     create_table :restaurants do |t|
-      t.string :name
+      t.string :name, null: false
       t.string :cuisine
       t.integer :rating
       t.string :details
-      t.string :address
-      t.float :longitude
-      t.float :latitude
+      t.string :address, null: false
+      t.float :longitude, null: false
+      t.float :latitude, null: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
