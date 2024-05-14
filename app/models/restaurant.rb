ca4231ab49c5f7f4394b11aa311d94b8091fc430
lawrence harlow <lawrenceharlow@gmail.com>
2024-05-14 14:21:17 +0100

    seed file now puts address into model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,10 +1,9 @@
 class Restaurant < ApplicationRecord
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
   belongs_to :user
   has_many :bookings
   validates :name, presence: true
   validates :cuisine, presence: true
-  validates :cuisine, presence: true
   validates :address, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
