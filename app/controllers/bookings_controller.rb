4fc7e36b05de3bc52ddd94070e0a6ba42b6aa78d
lawrence harlow <lawrenceharlow@gmail.com>
2024-05-15 15:29:05 +0100

    added in the count for individuals above the person in the queue

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,6 +2,15 @@ class BookingsController < ApplicationController
   before_action :set_booking, only: [:show, :new, :create]
   def index
     @bookings = Booking.where(user_id: 1)
+    #For each of the bookings return the results before the created date timestamp
+    @booking_infos = []
+    @bookings.each do |booking|
+      @booking_infos << {
+        booking: booking,
+        queue_position: Booking.where("restaurant_id=? AND created_at <= ? AND accepted = false",booking.restaurant_id, booking.created_at).count
+      }
+
+    end
   end
 
   def show
