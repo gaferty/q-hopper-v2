3aa92e8b4b452e8a3ab453a4766ddcad1e82bc4b
lawrence harlow <lawrenceharlow@gmail.com>
2024-05-17 14:48:17 +0100

    merged with master and tested

--- db/schema.rb
+++ db/schema.rb
@@ -41,17 +41,21 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_14_104024) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "restaurant_id"
+    t.bigint "user_id"
+    t.bigint "restaurant_id_id"
     t.integer "rating"
     t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["restaurant_id_id"], name: "index_reviews_on_restaurant_id_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", default: "", null: false
+    t.string "user_type", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
