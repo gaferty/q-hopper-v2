8a74d6f0cc1427e939b68ce1c34ed028e72a524e
Spartacus Naimi <spartacus.naimi@gmail.com>
2024-05-17 14:07:53 +0100

    This is the review page and review cards

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,15 @@
 class ReviewsController < ApplicationController
+  skip_before_action :authenticate_user!
   def review
     @reviews = Review.all
   end
+
+  def new
+    @reviews = Review.new
+  end
+
+  def create
+    @reviews = Review.create
+  end
+
 end
