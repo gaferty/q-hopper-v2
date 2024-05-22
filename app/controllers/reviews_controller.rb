0c867c0168056e99d582edad968fab2d88b8bf7b
Cyrus Devlin <cyrusdevlin@gmail.com>
2024-05-22 17:41:35 +0200

    Updating the cards for the reciews and completed tab

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,23 @@
 class ReviewsController < ApplicationController
-  skip_before_action :authenticate_user!
-  def review
-    @reviews = Review.all
-  end
+  skip_before_action :authenticate_user!, only: [:new, :create]
 
   def new
-    @reviews = Review.new
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 
   def create
-    @reviews = Review.create
+    @review = Review.new(review_params)
+    if @review.save
+      render json: @review, status: :created
+    else
+      render json: @review.errors, status: :unprocessable_entity
+    end
   end
 
+  private
+
+  def review_params
+    params.require(:review).permit(:user_id, :restaurant_id, :rating, :comment)
+  end
 end
