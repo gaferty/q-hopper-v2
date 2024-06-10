878abbd0f373ab594ac8e3701352f0048866aa87
lawrence harlow <lawrenceharlow@gmail.com>
2024-06-10 15:35:32 +0100

    set up the application to use an api

---
+++ q-hopper-backend/app/controllers/application_controller.rb
@@ -0,0 +1,21 @@
+class ApplicationController < ActionController::API
+  before_action :authenticate_user!
+
+  # Adding before_action to configure permitted parameters for Devise
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    # Redirect to the desired path after sign-in
+    restaurant_map_path # Assuming this is defined in your routes
+  end
+end
